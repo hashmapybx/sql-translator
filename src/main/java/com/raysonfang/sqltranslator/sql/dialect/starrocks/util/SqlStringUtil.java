367345446e907c36616b14924526c04b0493ebd3
hashmapybx <15868861416@163.com>
2025-01-09 15:19:31 +0800

    add oracle to starrocks primary key table for create sql

---
+++ src/main/java/com/raysonfang/sqltranslator/sql/dialect/starrocks/util/SqlStringUtil.java
@@ -0,0 +1,6 @@
+package com.raysonfang.sqltranslator.sql.dialect.starrocks.util;/**
+ * Date: 2025/1/9
+ * Author: rockyyin
+ * Description: 
+ */ public class SqlStringUtil {
+}
---
+++ src/test/java/com/raysonfang/sqltranslator/sql/OracleToSRsqlTest.java
@@ -0,0 +1,6 @@
+package com.raysonfang.sqltranslator.sql;/**
+ * Date: 2025/1/8
+ * Author: rockyyin
+ * Description: 
+ */ public class OracleToSRsqlTest {
+}
